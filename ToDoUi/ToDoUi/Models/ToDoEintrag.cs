1fa99cff8ed75e487152006e59b56c6bdfaf2467
F.Beck <f.beck@slx.de>
2025-02-12 08:26:26 +0100

    first commit

---
+++ ToDoUi/ToDoUi/Models/ToDoEintrag.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoUi.Models
+{
+    public class ToDoEintrag
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Beschreibung { get; set; }
+
+        public Guid Liste_id { get; set; }
+    }
+}
